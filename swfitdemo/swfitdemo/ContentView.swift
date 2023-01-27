b927745bc2af6553b0c4416b3a73b5e3931133b5
firehydrangea <57843561+firehydrangea@users.noreply.github.com>
2023-01-27 15:24:30 -0500

    Update ContentView.swift
    part 2

--- swfitdemo/swfitdemo/ContentView.swift
+++ swfitdemo/swfitdemo/ContentView.swift
@@ -9,10 +9,29 @@ import SwiftUI
 import Foundation
 import MapKit
 
+struct IdentifiablePlace: Identifiable {
+    let id: UUID
+    
+    let location: CLLocationCoordinate2D
+    let name: String
+    
+    init(id: UUID = UUID(), name: String, lat: Double, long: Double){
+        self.id = id
+        self.name = name
+        self.location = CLLocationCoordinate2D(
+            latitude: lat,
+            longitude: long)
+    }
+    
+}
+
 
 struct ContentView: View {
     //@EnvironmentObject private var vm: LocationsViewModel
     //@State private var mapRegion: MKCoordinateRegion = MKCoordinateRegion(
+    let places = [
+        IdentifiablePlace(name: "Dupre", lat: 40.2941_775, long: -79.4001)]
+    
     @State private var region = MKCoordinateRegion(
 
         center: CLLocationCoordinate2D(latitude: 40.2942, longitude: -79.4001),
@@ -21,10 +40,17 @@ struct ContentView: View {
     var body: some View {
         ZStack{
             //Map(coordinateRegion: $mapRegion)
-            Map(coordinateRegion: $region)
+            Map(coordinateRegion: $region,
+            annotationItems: places)
+            { place in
+                MapMarker(coordinate: place.location)
+            }
                 .ignoresSafeArea()
             Text("Saint Vincent College")
                 .font(.title)
+        }
+        .onTapGesture{
+            print(places[0].name)
         }
     }
 }
@@ -34,6 +60,7 @@ struct mapview_Previews: PreviewProvider {
         Group {
             ContentView()
                 .previewDevice("iPhone 11")
+            
         }
     }
 }
