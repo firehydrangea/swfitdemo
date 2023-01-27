4aee4a7a1e369c4bf8eaf032075fe5cdff7c0111
firehydrangea <57843561+firehydrangea@users.noreply.github.com>
2023-01-27 14:04:25 -0500

    Update ContentView.swift
    part 1 commit

--- swfitdemo/swfitdemo/ContentView.swift
+++ swfitdemo/swfitdemo/ContentView.swift
@@ -6,16 +6,34 @@
 //
 
 import SwiftUI
+import Foundation
+import MapKit
+
 
 struct ContentView: View {
+    //@EnvironmentObject private var vm: LocationsViewModel
+    //@State private var mapRegion: MKCoordinateRegion = MKCoordinateRegion(
+    @State private var region = MKCoordinateRegion(
+
+        center: CLLocationCoordinate2D(latitude: 40.2942, longitude: -79.4001),
+        span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            //Map(coordinateRegion: $mapRegion)
+            Map(coordinateRegion: $region)
+                .ignoresSafeArea()
+            Text("Saint Vincent College")
+                .font(.title)
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct mapview_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewDevice("iPhone 11")
+        }
     }
 }
