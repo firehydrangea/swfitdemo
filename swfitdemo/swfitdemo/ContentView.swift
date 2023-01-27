a5411b0e2e8f117a121f8eb7bfe70ff0254ac1a1
firehydrangea <57843561+firehydrangea@users.noreply.github.com>
2023-01-27 14:00:07 -0500

    initial
    ignore; this is just the empty swiftui project.

---
+++ swfitdemo/swfitdemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swfitdemo
+//
+//  Created by Sarah Kudrick on 1/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swfitdemo/swfitdemo/swfitdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swfitdemoApp.swift
+//  swfitdemo
+//
+//  Created by Sarah Kudrick on 1/27/23.
+//
+
+import SwiftUI
+
+@main
+struct swfitdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
